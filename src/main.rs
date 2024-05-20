a051b61a47662e91e2d5abfa8d0cfce7397d95f0
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-05-19 21:56:44 -0400

    chip must be mutable

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use linux_embedded_hal::gpio_cdev::Chip;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let chip = Chip::new("/dev/gpiochip0")?;
+    let mut chip = Chip::new("/dev/gpiochip0")?;
 
     return Ok(());
 }
