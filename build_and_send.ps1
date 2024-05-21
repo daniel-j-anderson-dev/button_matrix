ed35aea9e41c2de8d413459d6e8cc1a4e12e7ec4
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2024-05-21 15:59:18 -0400

    made simple scripts to simplify testing

---
+++ build_and_send.ps1
@@ -0,0 +1,2 @@
+cross build --target aarch64-unknown-linux-gnu;
+.\send_to_orangepi.ps1
---
+++ send_to_orangepi.ps1
@@ -0,0 +1,1 @@
+scp ".\target\aarch64-unknown-linux-gnu\debug\button_matrix" daniel@192.168.1.90:"~/code/rust/button_matrix/button_matrix"
